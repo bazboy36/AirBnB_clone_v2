4f2e64116019f622be83b48a4869277347ed40b1
Davies70 <daviesajayi7@gmail.com>
2022-10-28 04:12:04 -0700

    second

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,6 +8,7 @@ import datetime
 
 
 def do_pack():
+    """ creates a tar.tgz compressed file """
     date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     arg = 'versions/web_static_{}.tgz'.format(date)
     result = local(f'mkdir -p versions && tar -cvzf {arg} web_static')
