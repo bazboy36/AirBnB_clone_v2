19e6fcec4ae7543039f99e5133895e42bea0c1c5
Davies70 <daviesajayi7@gmail.com>
2022-10-28 05:11:32 -0700

    $desc

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,15 @@
         using the function do_pack. """
 
 from fabric.api import *
+from fabric.decorators import runs_once
 import datetime
 
-
+@runs_once
 def do_pack():
-    ''' creates a tar.tgz compressed file p '''
+    ''' creates a .tgz archive from the contents of web_static folder '''
     date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    arg = 'versions/web_static_{}.tgz'.format(date)
-    result = local(f'mkdir -p versions && tar -cvzf {arg} web_static')
-    if result.return_code == 0:
-        return arg
-    else:
-        return None
+    path = 'versions/web_static_{}.tgz'.format(date)
+    result = local(f'mkdir -p versions && tar -cvzf {path} web_static')
+    if result.failed:
+        return path
+    return None
