71abf8c10cb98bb953ced2fdc16377ab95270153
bazboy36 <shokunbiabdulbasit1@gmail.com>
2022-11-14 09:15:27 -0800

    5 and 7 lists

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
-"""Module for the different pages with Flask"""
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 14:42:23 2020
+@author: Robinson Montes
+"""
 from flask import Flask
 from flask import render_template
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """return the message for the index"""
+def hello():
+    """Start a basic Flask web application"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """return the message for /hbnb"""
+    """Adding a specific route /hbnb"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def message_c(text):
-    """custom message for c"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text=None):
+    """Dynamic inputed text: C + replace _ for space and show text"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/<text>', strict_slashes=False)
 @app.route('/python', strict_slashes=False)
-def message_python(text='is cool'):
-    """custom message for python"""
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Dynamic inputed text: Python + replace _ for space and show text"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def message_number(n):
-    """Custom message for /number"""
-    return '{} is a number'.format(n)
+def only_digits_dynamic(n=None):
+    """Dynamic inputted integer"""
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def template_number(n):
-    """Return a template"""
-    return render_template('5-number.html', number=n)
+def first_template(n=None):
+    """Display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
